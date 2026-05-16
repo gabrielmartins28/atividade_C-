1c5e5168f07bffaeb49fce0f77b0e2d93ecfe11a
gabrielmartins28 <gabriel_martins28@estudante.sesisenai.org.br>
2026-05-15 21:28:09 -0300

    15-05

---
+++ 1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]){
+
+float preco (q_estoque, unitario):
+    return q_estoque + unitario;
+
+char[50] = nome;
+int = q_estoque;
+float = unitario;
+
+printf("Digite o seu nome:");
+scanf("%s", &nome);
+
+printf("Digite a quantidade em estoque:");
+scanf("%d", &q_estoque);
+
+printf("Agora o preço do produto:");
+scanf("%f", %unitario);
+
+ 
+
+
+return 0
+}
+
+
